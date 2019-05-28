84a165cb7c75433cedebea7b0d572ed5514f3fcc
Romane G <romane71193@gmail.com>
2019-05-27 21:39:53 -0400

    clean up

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,13 +2,6 @@ import React from 'react'
 
 const Button = (props) => {
 
-    // pushButton = () => {
-
-    //     const newInput = this.state.input.push()
-    //       this.setState = ({
-    //         input: newInput
-    //       })
-    //   }
 
   return (
     <div>
