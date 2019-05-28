23378b2163a1408d9ef9c58900b0b2fd39299cfe
Romane G <romane71193@gmail.com>
2019-05-27 20:20:06 -0400

    handle input,app constructor

--- src/App.js
+++ src/App.js
@@ -5,45 +5,61 @@ import Clear from './components/Clear'
 import './App.css';
 
 function App() {
+
+      constructor(props){
+        super(props)
+        this.state={
+            input: ''
+        }
+
+        handleInput = (digit) => {
+
+          const newInput = this.state.input.push(digit)
+            this.setState = ({
+              input: newInput
+            })
+        }
+      }
+
   return (
     <div className="App allCalc">
-      
+
      <div className="rowz"><Input />
       </div>
 
       <div className="rowz">
-    <Button>7</Button>
-    <Button>8</Button>
-    <Button>9</Button>
+    <Button onClick={this.handleInput(digit)}>7</Button>
+    <Button onClick={this.handleInput(digit)}>8</Button>
+    <Button onClick={this.handleInput(digit)}>9</Button>
     </div>
 
     <div className="rowz">
-    <Button>4</Button>
-    <Button>5</Button>
-    <Button>6</Button>
+    <Button onClick={this.handleInput(digit)}>4</Button>
+    <Button onClick={this.handleInput(digit)}>5</Button>
+    <Button onClick={this.handleInput(digit)}>6</Button>
     </div>
 
     <div className="rowz">
-    <Button>1</Button>
-    <Button>2</Button>
-    <Button>3</Button>
+    <Button onClick={this.handleInput(digit)}>1</Button>
+    <Button onClick={this.handleInput(digit)}>2</Button>
+    <Button onClick={this.handleInput(digit)}>3</Button>
     </div>
 
     <div className="rowz">
-    <Button>0</Button>
-    <Button>.</Button>
+    <Button onClick={this.handleInput(digit)}>0</Button>
+    <Button onClick={this.handleInput(digit)}>.</Button>
     
-    <Button>/</Button>
+    <Button onClick={this.handleInput(digit)}>/</Button>
     </div>
     
     <div className="rowz">
-    <Button>+</Button>
-    <Button>-</Button>
-    <Button>*</Button>
+    <Button onClick={this.handleInput(digit)}>+</Button>
+    <Button onClick={this.handleInput(digit)}>-</Button>
+    <Button onClick={this.handleInput(digit)}>*</Button>
     </div>
 
     <div className="rowz">
-    <Button>=</Button>
+    <Button onClick={this.handleInput(digit)}>=</Button>
   <Clear>Clear</Clear>
   </div>
     </div>
